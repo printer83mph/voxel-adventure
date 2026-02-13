bf5e1caef452a85da0e30d7d1d5242301d06d3b0
Thomas Shaw <printer.83mph@gmail.com>
2026-02-13 21:03:28 +0000

    Add panning to orbit camera

--- libs/vxng/src/scene/orbit-camera.cpp
+++ libs/vxng/src/scene/orbit-camera.cpp
@@ -6,24 +6,24 @@
 namespace vxng::scene {
 
 OrbitCamera::OrbitCamera(glm::vec3 target, glm::vec3 angle_euler_yxz,
-                         float distance, float sensitivity, float min_x,
-                         float max_x)
-    : Camera(), target(target), angle_euler_yxz(angle_euler_yxz),
-      distance(distance) {
-    settings.sensitivity = sensitivity;
-    settings.min_x = min_x;
-    settings.max_x = max_x;
+                         float distance, float fovy_rad)
+    : Camera(glm::vec3(0.f), glm::mat3(1.f), fovy_rad), target(target),
+      angle_euler_yxz(angle_euler_yxz), distance(distance) {
+    this->update_camera();
 }
 
 OrbitCamera::OrbitCamera()
-    : OrbitCamera(glm::vec3(0), glm::vec3(0.7f, 0.5f, 0.f), 8.f, 0.1f,
-                  glm::pi<float>(), -glm::pi<float>()) {};
+    : OrbitCamera(glm::vec3(0), glm::vec3(0.7f, 0.5f, 0.f), 8.f, 0.7f) {};
 
 OrbitCamera::~OrbitCamera() = default;
 
 auto OrbitCamera::on_rotate(float dx, float dy) -> void {
-    this->angle_euler_yxz.y += dx * this->settings.sensitivity;
-    this->angle_euler_yxz.x += dy * this->settings.sensitivity;
+    // clamp x rotation to not go upside down
+    this->angle_euler_yxz.x = glm::clamp(
+        this->angle_euler_yxz.y + dy * this->settings.rotate_sensitivity,
+        this->settings.min_x, this->settings.max_x);
+
+    this->angle_euler_yxz.y += dx * this->settings.rotate_sensitivity;
 
     this->update_camera();
     this->update_gl();
@@ -31,6 +31,16 @@ auto OrbitCamera::on_rotate(float dx, float dy) -> void {
 
 auto OrbitCamera::on_zoom(float delta) -> void {
     this->distance -= delta;
+
+    this->update_camera();
+    this->update_gl();
+}
+
+auto OrbitCamera::on_pan(float dx, float dy) -> void {
+    glm::vec3 right = this->rotation[0];
+    glm::vec3 up = this->rotation[1];
+
+    this->target += (right * dx + up * dy) * this->settings.pan_sensitivity;
 
     this->update_camera();
     this->update_gl();
--- libs/vxng/src/scene/orbit-camera.h
+++ libs/vxng/src/scene/orbit-camera.h
@@ -9,20 +9,23 @@ namespace vxng::scene {
 class OrbitCamera : vxng::scene::Camera {
   public:
     OrbitCamera(glm::vec3 target, glm::vec3 angle_euler_yxz, float distance,
-                float sensitivity, float min_x, float max_x);
+                float fovy_rad);
     OrbitCamera();
     ~OrbitCamera();
 
     auto on_rotate(float dx, float dy) -> void;
     auto on_zoom(float delta) -> void;
+    auto on_pan(float dx, float dy) -> void;
 
-    /** Tweakable settings */
+    /** Tweakable settings for consumers */
     struct {
-        float sensitivity;
-        float min_x, max_x;
+        float rotate_sensitivity = 0.05f;
+        float min_x = glm::pi<float>(), max_x = -glm::pi<float>();
+        float pan_sensitivity = 0.05f;
     } settings;
 
   private:
+    /** Update base `Camera` class protected parameters */
     auto update_camera() -> void;
 
     glm::vec3 target;

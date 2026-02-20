6ea0f7bd44d650a4e80e38a200ac366d71395ea8
Thomas Shaw <printer.83mph@gmail.com>
2026-02-19 21:15:45 -0500

    Add chunk map and init_webgpu method to chunk

--- libs/vxng/src/scene/scene.cpp
+++ libs/vxng/src/scene/scene.cpp
@@ -1,8 +1,19 @@
 #include "vxng/scene.h"
 
+#include "chunk.h"
+
+#include <memory>
+
 namespace vxng::scene {
 
 Scene::Scene() {}
 Scene::~Scene() {}
+
+auto Scene::init_webgpu(wgpu::Device device) -> void {
+    auto origin_chunk = std::make_unique<Chunk>(glm::vec3(0), 4.0, 512);
+    origin_chunk->init_webgpu(device);
+
+    this->chunks[glm::ivec3(0)] = std::move(origin_chunk);
+}
 
 } // namespace vxng::scene

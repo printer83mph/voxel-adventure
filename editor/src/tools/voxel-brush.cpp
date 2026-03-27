f4d4699effdf1c368cf45a20ebd23025ceb5b761
Thomas Shaw <printer.83mph@gmail.com>
2026-03-26 22:19:06 -0400

    fix: disable involuntary voxel brush triggers

--- editor/src/tools/voxel-brush.cpp
+++ editor/src/tools/voxel-brush.cpp
@@ -1,4 +1,5 @@
 #include "voxel-brush.h"
+
 #include "cursors.h"
 
 VoxelBrush::VoxelBrush() {}
@@ -14,6 +15,15 @@ auto VoxelBrush::render_ui() -> void {}
 
 auto VoxelBrush::handle_mouse_button_event(const MouseButtonEventBundle &bundle)
     -> void {
+    // we don't care about mouse up
+    if (!bundle.event->down)
+        return;
+
+    // don't do anything if any mods are held
+    SDL_Keymod mods = SDL_GetModState();
+    if (mods != SDL_KMOD_NONE)
+        return;
+
     auto mouse_ray = bundle.camera->screen_to_ray(bundle.mouse_ndc_coords);
     auto raycast_result = bundle.scene->raycast(mouse_ray);
 
@@ -25,12 +35,11 @@ auto VoxelBrush::handle_mouse_button_event(const MouseButtonEventBundle &bundle)
     glm::vec3 target_pos =
         mouse_ray.origin + raycast_result.t * mouse_ray.direction;
     // go outside of voxel boundary
-    glm::vec3 exterior_target_pos =
-        target_pos + raycast_result.normal * 0.0001f;
+    glm::vec3 exterior_target_pos = target_pos + raycast_result.normal * 0.001f;
 
     // set voxel filled!
     bundle.scene->set_voxel_filled(9, exterior_target_pos,
-                                   glm::u8vec4(255, 255, 255, 255));
+                                   glm::u8vec4(255, 0, 0, 255));
 }
 
 auto VoxelBrush::handle_mouse_motion_event(const MouseMotionEventBundle &bundle)

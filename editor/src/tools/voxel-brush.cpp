2933fa930cb06fa279ae4633a513fd77c0938144
Thomas Shaw <printer.83mph@gmail.com>
2026-03-26 21:52:10 -0400

    Add voxel brush tool implementation

---
+++ editor/src/tools/voxel-brush.cpp
@@ -0,0 +1,49 @@
+#include "voxel-brush.h"
+#include "cursors.h"
+
+VoxelBrush::VoxelBrush() {}
+
+VoxelBrush::~VoxelBrush() {}
+
+auto VoxelBrush::get_tool_name() -> const char * {
+    return "Simple Voxel Brush";
+}
+
+// no ui for this yet
+auto VoxelBrush::render_ui() -> void {}
+
+auto VoxelBrush::handle_mouse_button_event(const MouseButtonEventBundle &bundle)
+    -> void {
+    auto mouse_ray = bundle.camera->screen_to_ray(bundle.mouse_ndc_coords);
+    auto raycast_result = bundle.scene->raycast(mouse_ray);
+
+    // if nothing hit, do nothing
+    if (raycast_result.t < 0) {
+        return;
+    }
+
+    glm::vec3 target_pos =
+        mouse_ray.origin + raycast_result.t * mouse_ray.direction;
+    // go outside of voxel boundary
+    glm::vec3 exterior_target_pos =
+        target_pos + raycast_result.normal * 0.0001f;
+
+    // set voxel filled!
+    bundle.scene->set_voxel_filled(9, exterior_target_pos,
+                                   glm::u8vec4(255, 255, 255, 255));
+}
+
+auto VoxelBrush::handle_mouse_motion_event(const MouseMotionEventBundle &bundle)
+    -> void {
+    auto mouse_ray = bundle.camera->screen_to_ray(bundle.mouse_ndc_coords);
+    auto raycast_result = bundle.scene->raycast(mouse_ray);
+
+    if (raycast_result.t >= 0) {
+        bundle.cursors->set_cursor(Cursors::Variant::POINTER);
+    } else {
+        bundle.cursors->set_cursor(Cursors::Variant::DEFAULT);
+    }
+}
+
+auto VoxelBrush::handle_keyboard_event(const KeyboardEventBundle &bundle)
+    -> void {}
